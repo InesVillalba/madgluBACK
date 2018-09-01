f6672959c263430c89fbbed94a3e8805766d471d
Inés Villalba <paprika.sputnik@gmail.com>
2018-09-01 15:39:58 +0200

    Añadidos datos a la DDBB. Creación de filtros dinámicos. Prueba de filtro.

--- models/restaurants.js
+++ models/restaurants.js
@@ -10,24 +10,53 @@ exports.getAll = (done) => {
 }
 
 exports.getCategories = (done) =>{
-    db.get().query('SELECT * FROM categorie', (err, rows) => {
+    db.get().query('SELECT * FROM categorie order by value asc', (err, rows) => {
+        if(err) return done(err.message);
+        done(null, rows);
+    })
+}
+
+exports.getAreas = (done) =>{
+    db.get().query('SELECT distinct area FROM restaurants order by area asc', (err, rows) => {
         if(err) return done(err.message);
         done(null, rows);
     })
 }
 
 exports.getRestAndCategories = (done) => {
-    db.get().query('select r.id as restId, r.name as restName, r.description as restDescription, r.area as restArea, r.address as restAddress, c.name as tagName from restaurants r, categorie c, rest_tags rt where r.id = rt.id_rest and c.id = rt.id_categorie;', (err, rows) => {
+    db.get().query('select r.id, r.name, r.description, r.area, r.address, GROUP_CONCAT(c.name SEPARATOR ",") as tags '
+                    +'from restaurants r '
+                    +'inner join rest_tags rt on r.id = rt.id_rest '
+                    +'inner join categorie c on c.id = rt.id_categorie '
+                    +'group by 1,2,3,4,5'
+                , (err, rows) => {
         if(err) return done(err.message);
         done(null, rows);
     })
 }
-
 
 exports.getById = (idRestaurant, done) => {
     db.get().query('SELECT * FROM restaurants WHERE id = ?', [idRestaurant], (err, rows) => {
         if(err) return done(err.message);
         done(null, rows);
     })
+}
+/*
+exports.getFilteredRestaurants = (areas, categories, done) => {
+    db.get().query("select r.id, r.name, r.description, r.area, r.address, GROUP_CONCAT(c.name SEPARATOR ',') as tags "
+                    +"from restaurants r "
+                    +"inner join rest_tags rt on r.id = rt.id_rest "
+                    +"inner join categorie c on c.id = rt.id_categorie "
+                    +"where "+ areas != null ? "r.area in ("+areas+") " : "1 "
+                    +"and "+ categories != null ? "c.value in ("+categories+") " : "1 "                   
+                    +"group by 1,2,3,4,5"
+                , (err, rows) => {
+        if(err) return done(err.message);
+        done(null, rows);
+    })
+}*/
+
+exports.filter = (done) => {
+    db.get().query('SELECT * FROM restaurants WHERE ')
 }
 
--- routes/api/restaurants.js
+++ routes/api/restaurants.js
@@ -6,31 +6,41 @@ const modelRestaurants = require('../../models/restaurants.js');
 // GET http://localhost:3000/api/restaurants/
 router.get('/', (req, res) => {
     modelRestaurants.getRestAndCategories((err, rows) => {
-        let restaurantes = [];
-        rows.forEach(row => {
-            // Si en el array restaurantes existe un objeto que coincida con el id de la fila que estamos iterando con el foreach (row) devuelve ese objeto en restFound
-            let restFound = restaurantes.find((rest) =>{                            
-                return row.restId === rest.id
-            })
-            
-            if(!restFound){
-                restaurantes.push({
-                    id: row.restId,
-                    name: row.restName,
-                    description: row.restDescription,
-                    area: row.restArea,
-                    address: row.restAddress, 
-                    tags: [row.tagName]
-                })
-            }else{
-                restFound.tags.push(row.tagName)
-            }
-        });
-        res.json(restaurantes)
+      res.json(rows);
     })
 }) 
 
-// GET http://localhos:3000/api/restaurants/3
+// GET http://localhost:3000/api/restaurants/categories
+router.get('/categories', (req, res) => {    
+    modelRestaurants.getCategories((err, rows) => {  
+        res.json(rows);
+    })
+})
+
+// GET http://localhost:3000/api/restaurants/areas
+router.get('/areas', (req, res) => {    
+    modelRestaurants.getAreas((err, rows) => {  
+        res.json(rows);
+    })
+})
+
+/*
+// GET http://localhost:3000/api/restaurants/filter?areas=Arganzuela,Centro&categories=cafe,italiano
+router.get('/filter', (req, res) => {    
+    //console.log("FILTER: "+req.params);
+    
+    //let myAreas = req.param.filters.areas === "" ? null : req.param.filters.areas;
+    //let myCategories = req.param.filters.caregories === "" ? null : req.param.filters.categories;
+
+    modelRestaurants.getFilteredRestaurants("'Arganzuela','Centro'", "'cafe','italiano'", (err, rows) => {
+        if(err) console.log('No se ha encontrado un restaurante con ese ID');
+        if(rows.length == 1){
+            res.json(rows);
+        }
+    })
+})
+*/
+// GET http://localhost:3000/api/restaurants/3
 router.get('/:idRestaurant', (req, res) => {
     modelRestaurants.getById(req.params.idRestaurant, (err, rows) => {
         if(err) console.log('No se ha encontrado un restaurante con ese ID');
@@ -39,6 +49,5 @@ router.get('/:idRestaurant', (req, res) => {
         }
     })
 })
-
 
 module.exports = router;
