5beda8cfa747757fab41eabe5a5453a091d34811
Inés Villalba <paprika.sputnik@gmail.com>
2018-09-10 18:24:20 +0200

    Login funcionando

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,19 +4,18 @@ var router = express.Router();
 const modelUsers = require('../../models/users.js')
 
 router.post('/registro', (req, res) => {
-    console.log(req.body)
+    console.log(req.body);
     modelUsers.newUser(req.body, (err, rows) =>{
-        console.log(err)
+        console.log(err);
         res.json(rows);
     })
 })
 
-router.get('/login', (req, res) => {
-    console.log("LOGIN INFO: ");
+router.post('/login', (req, res) => {
     console.log(req.body);
     modelUsers.getUser(req.body, (err, rows) =>{
-        console.log(err)
-        console.log(res.json(rows));
+        console.log(err);
+        res.json(rows[0]);
     })
 })
 
